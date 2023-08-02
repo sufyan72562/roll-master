b37a39baad523be46bdd364a70c4385cba262485
muhammadsufyan-cheetay <muhammad.sufyan@cheetay.pk>
2023-08-02 10:04:46 +0500

    update serializers changes

--- rollchat/serializers.py
+++ rollchat/serializers.py
@@ -7,27 +7,43 @@ class MessageSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Message
-        fields = ['id', 'sender', 'text', 'timestamp', 'is_seen']
+        fields = ['id', 'sender', 'text', 'timestamp', 'is_seen', 'image']
 
     def get_sender(self, obj):
         return {
             "sender_name": obj.sender.firstname + " " + obj.sender.lastname,
-            "sender_image": obj.sender.image.url
+            "sender_image": obj.sender.image.url,
+            "sender_id": obj.sender.id
         }
 
 
 class ConversationSerializer(serializers.ModelSerializer):
-    # messages = MessageSerializer(many=True, read_only=True)
     last_message = serializers.SerializerMethodField()
+    other_participant = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
-        fields = ['id', 'participants', 'last_message']
+        fields = ['id', 'participants', 'last_message', 'other_participant']
 
     def get_last_message(self, obj):
-        last_message = obj.messages.order_by('-timestamp').first()
+        last_message = obj.messages.last()  # Directly access the last message from the prefetched data
         if last_message:
             serializer = MessageSerializer(last_message)
             return serializer.data
         else:
             return None
+
+    def get_other_participant(self, obj):
+        request_user = self.context['request'].user
+        participants = obj.participants.all()
+        for participant in participants:
+            if participant != request_user:
+                return {
+                    "participant_name": participant.firstname + " " + participant.lastname,
+                    "participant_image": participant.image.url,
+                    "participant_id": participant.id
+                }
+        return None
+
+
+
